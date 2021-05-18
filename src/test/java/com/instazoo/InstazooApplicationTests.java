7f995b08356e26b19e6b9138aebaeb1ac258fc4c
bryahchin.mihail <bryahchin.mihail@otr.ru>
2021-05-18 21:43:14 +0300

    Initial commit

---
+++ src/test/java/com/instazoo/InstazooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instazoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
